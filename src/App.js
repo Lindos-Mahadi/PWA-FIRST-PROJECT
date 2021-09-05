b7a3ebe4502179ab7beb32a1bce2abf04dc45137
Lindos-Mahadi <lindosmahadi@.com>
2021-09-05 19:25:51 +0600

    Navbar & Components SetUp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import Home from './components/Home'
+import About from './components/About'
+import Users from './components/Users'
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link ><Link to="/">Home</Link></Nav.Link>
+                  <Nav.Link ><Link to="/about" >About</Link></Nav.Link>
+                  <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+            </Nav>
+            </Container>
+        </Navbar>
+          
+        <Switch>
+          <Route path="/about" component={About} ></Route>
+          <Route path="/users" component={Users} ></Route>
+          <Route path="/" component={Home} ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Users() {
+    return (
+        <div>
+            <h1 className="bg-secondary">Users Componet</h1>
+        </div>
+    )
+}
+
+
+export default Users
