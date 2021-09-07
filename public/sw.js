554670539c4eb45ac62f6ab7bbf6ac987bda22c3
Lindos-Mahadi <lindosmahadi@.com>
2021-09-07 20:33:12 +0600

    offline Mode SetUp

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,35 @@
 console.warn("sw from public forlder")
+
+let cacheData = "appV1";
+
+this.addEventListener("install", (event) =>{
+    event.waitUntil(
+        caches.open(cacheData).then((cache)=>{
+            cache.addAll([
+                "/static/js/bundle.js",
+                "/static/js/main.chunk.js",
+                // "/static/js/0.chunk.js",
+                "/static/js/vendors~main.chunk.js",
+                "/index.html",
+                "/",
+                "/users",
+                "/about"
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    // if(!navigator.online)
+    // {
+        event.respondWith(
+            caches.match(event.request).then((result) =>{
+                if(result)
+                {
+                    return result
+                }
+            })
+        )
+    // }
+    
+})
--- src/App.js
+++ src/App.js
@@ -1,33 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Navbar, Nav, Container } from 'react-bootstrap'
-import Home from './components/Home'
-import About from './components/About'
-import Users from './components/Users'
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link ><Link to="/">Home</Link></Nav.Link>
-                  <Nav.Link ><Link to="/about" >About</Link></Nav.Link>
-                  <Nav.Link><Link to="/users">Users</Link></Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
-          
-        <Switch>
-          <Route path="/about" component={About} ></Route>
-          <Route path="/users" component={Users} ></Route>
-          <Route path="/" component={Home} ></Route>
-        </Switch>
-      </Router>
-    </div>
+import React from 'react';
+import "./App.css";
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Users from "./components/Users";
+import Navbar from "./components/Navbar";
+
+
+const App = () =>{
+  return(
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/users" component={Users} />
+        <Route exact path="/" component={Home} />
+        <Redirect to="/" />
+      </Switch>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
